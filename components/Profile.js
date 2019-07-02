d41e5c457efec2b8522344aa4d09ef7412774b1e
abdelrahman84 <abdelrahman84>
2019-07-02 11:19:36 +0200

    adding current Kilos

--- components/Profile.js
+++ components/Profile.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-native'
+import { View, TextInput, Text, StyleSheet, Button, TouchableOpacity, Image } from 'react-native'
 import { connect } from 'react-redux'
 import Firebase from '../config/Firebase'
 
 class Profile extends React.Component {
+	state = {
+		currentKilos: 0
+	}
+
+
 	handleSignout = () => {
 		Firebase.auth().signOut()
 		this.props.navigation.navigate('Login')
@@ -14,6 +19,18 @@ class Profile extends React.Component {
 			<View style={styles.container}>
 				<Text h1 style={styles.header}>Profile Screen</Text>
 				<Text h2 style={styles.header}>Welcome {this.props.user.userName}</Text>
+
+				<Text h3 style={styles.header}>Current Kilos {this.state.currentKilos}</Text>
+
+				<TextInput
+				    keyboardType="numeric"
+					style={styles.inputBox}
+					value={`${this.state.currentKilos}`}
+                    onChangeText={currentKilos => this.setState({ currentKilos })}
+                    placeholder='current Kilos'
+                    />
+
+			
 
 				<TouchableOpacity style={styles.button} onPress={this.handleSignout}>
 					<Text style={styles.buttonText}>Logout</Text>
@@ -51,6 +68,15 @@ const styles = StyleSheet.create({
 		color: '#fff',
 
 	},
+	inputBox: {
+        width: '50%',
+        margin: 10,
+        padding: 15,
+        fontSize: 16,
+        borderColor: '#d3d3d3',
+        borderBottomWidth: 1,
+		textAlign: 'center'
+	}
 })
 
 const mapStateToProps = state => {
